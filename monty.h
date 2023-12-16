27135cb52d5f4228e82f5c1148d6be4a978803be
umuhozalionel <muhozalionel@gmail.com>
2023-12-16 08:28:50 -0800

    Monty repo

--- monty.h
+++ monty.h
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -22,6 +23,7 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
+
 /**
  * struct bus_s - variables -args, file, line content
  * @arg: value
